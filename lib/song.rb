c86f222f08641a190aea00594ba0b513769ee14a
Seth Near <seth.j.near@gmail.com>
2020-06-23 16:07:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,8 +23,10 @@ class Song
   end
 
 def self.create_by_name(name)
-  new_song = self.new_by_name(name)
+  new_song = self.new
+  new_song.name = name
   new_song.save
+  return new_song
 end
 
 def self.find_by_name(name)
